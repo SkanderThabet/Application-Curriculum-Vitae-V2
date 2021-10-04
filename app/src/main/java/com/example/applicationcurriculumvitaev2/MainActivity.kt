99eb1c14e3429f8babb7d2a94b2718790c8f2ad0
Skander THABET <Skander.THABET@polytechnicien.tn>
2021-10-04 22:07:24 +0100

    Added intent for the result and Skill hobies plus transition via buttons

--- app/src/main/java/com/example/applicationcurriculumvitaev2/MainActivity.kt
+++ app/src/main/java/com/example/applicationcurriculumvitaev2/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Patterns
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.RadioGroup
@@ -19,64 +20,76 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val name = findViewById<TextView>(R.id.NameInput)
-        val outlineName= findViewById<TextInputLayout>(R.id.outlined_username)
+        val outlineName = findViewById<TextInputLayout>(R.id.outlined_username)
         val email = findViewById<TextView>(R.id.emailInput)
-        val outlineEmail= findViewById<TextInputLayout>(R.id.outlined_email)
+        val outlineEmail = findViewById<TextInputLayout>(R.id.outlined_email)
         val age = findViewById<TextView>(R.id.ageInput)
-        val outlineAge= findViewById<TextInputLayout>(R.id.outlined_age)
-        val next= findViewById<Button>(R.id.next)
-        val male= findViewById<RadioButton>(R.id.genre_homme)
-        val female= findViewById<RadioButton>(R.id.genre_femme)
+        val outlineAge = findViewById<TextInputLayout>(R.id.outlined_age)
+        val next = findViewById<Button>(R.id.next)
+        val male = findViewById<RadioButton>(R.id.genre_homme)
+        val female = findViewById<RadioButton>(R.id.genre_femme)
         val GenderGroup = findViewById<RadioGroup>(R.id.GenderGroup)
         val groupe = GenderGroup.checkedRadioButtonId
-        val FM= findViewById<RadioButton>(groupe)
+        val FM = findViewById<RadioButton>(groupe)
 
+        when {
+            email.text.isEmpty() -> next.isEnabled = false
+            age.text.isEmpty() -> next.isEnabled = false
+            name.text.isEmpty() -> next.isEnabled = false
+
+        }
         name.doOnTextChanged { text, start, before, count ->
-            if(text!!.isEmpty()){
-                outlineName.error="Must not be empty!"
-                next.isEnabled=false
-            }
-            else{
-                outlineName.error=null
-                next.isEnabled=true
+            if (text!!.isEmpty()) {
+                outlineName.error = "Must not be empty!"
+                next.isEnabled = false
+            } else {
+                outlineName.error = null
+                next.isEnabled = !(email.text.isEmpty() && age.text.isEmpty())
             }
         }
         email.doOnTextChanged { text, start, before, count ->
-            if(text!!.isEmpty()){
-                outlineEmail.error="Must not be empty!"
-                next.isEnabled=false
-            }
-            else{
-                outlineEmail.error=null
-                next.isEnabled=true
+            when {
+                Patterns.EMAIL_ADDRESS.matcher(email.text.toString())
+                    .matches() -> {
+                    next.isEnabled = true
+                }
+                text!!.isEmpty() -> {
+                    outlineEmail.error = "Must not be empty!"
+                    next.isEnabled = false
+                }
+                else -> {
+                    email.setError("Check your mail")
+                    next.isEnabled = !(age.text.isEmpty() && name.text.isEmpty())
+
+
+                }
             }
         }
         age.doOnTextChanged { text, start, before, count ->
-            if(text!!.isEmpty()){
-                outlineAge.error="Must not be empty!"
-                next.isEnabled=false
-            }
-            else if (text.length >= 3){
-                outlineAge.error="Age is wrong!"
-                next.isEnabled=false
-            }
-            else{
-                outlineAge.error=null
-                next.isEnabled=true
+            if (text!!.isEmpty()) {
+                outlineAge.error = "Must not be empty!"
+                next.isEnabled = false
+            } else if (text.length >= 3) {
+                outlineAge.error = "Age is wrong!"
+                next.isEnabled = false
+            } else {
+                outlineAge.error = null
+                next.isEnabled = !(email.text.isEmpty() && name.text.isEmpty())
             }
         }
 
         next.setOnClickListener {
-            val fullname= name.text.toString()
-            val ageV2= age.text.toString()
-            val mail = email.text.toString()
-            val genre= FM.text.toString()
 
-            val intent = Intent(this,SkillHobbiesActivity::class.java)
-            intent.putExtra("Name",fullname)
-            intent.putExtra("Email",mail)
-            intent.putExtra("Age",ageV2)
-            intent.putExtra("Gender",genre)
+            val fullname = name.text.toString()
+            val ageV2 = age.text.toString()
+            val mail = email.text.toString()
+            val genre = FM.text.toString()
+
+            val intent = Intent(this, SkillHobbiesActivity::class.java)
+            intent.putExtra("Name", fullname)
+            intent.putExtra("Email", mail)
+            intent.putExtra("Age", ageV2)
+            intent.putExtra("Gender", genre)
             startActivity(intent)
 
         }
