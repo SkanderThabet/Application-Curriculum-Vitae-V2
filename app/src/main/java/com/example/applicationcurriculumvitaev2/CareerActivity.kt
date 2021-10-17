7e66742eda0e65e3e989321ff1b5c11907a4a08f
Skander THABET <Skander.THABET@polytechnicien.tn>
2021-10-17 21:15:38 +0100

    Need to add recycler view and it'll work

--- app/src/main/java/com/example/applicationcurriculumvitaev2/CareerActivity.kt
+++ app/src/main/java/com/example/applicationcurriculumvitaev2/CareerActivity.kt
@@ -2,10 +2,22 @@ package com.example.applicationcurriculumvitaev2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CareerActivity : AppCompatActivity() {
+
+    private var layoutManager : RecyclerView.LayoutManager?=null
+    private var adapter: RecyclerView.Adapter<AdapterExperience.ViewHolder>?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_career)
+
+    TODO("Need to add the recycler view and the layoutManager")
+        layoutManager = LinearLayoutManager(this)
+
     }
+
+
 }
